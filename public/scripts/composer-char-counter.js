00e2684eeec7d14090072c8b06997f4bbd29ebc2
Cher <red_phoenix417@yahoo.com>
2019-06-11 23:17:52 +0000

    Added a character-counting for the tweet and a container that will be used to contain previous tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+
+console.log("The document is ready")
+
+
+
+  $("textarea").focus(function(){
+    $(this).css("background-color", "#d7f2f2");
+  });
+
+  $("textarea").blur(function(){
+    $(this).css("background-color", "#ffffff"	);
+  });
+
+
+
+  $("main section form textarea").on("input", function(){
+      let count = (140 - this.value.length);
+      console.log(count)
+
+    $("main section form span.counter").text(count);
+
+    if (count < 0) {
+      $(".counter").addClass("red");
+    } else {
+      $(".counter").removeClass("red")
+    }
+
+  });
+
+
+});
