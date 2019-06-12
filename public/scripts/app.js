03e4226a99ad078043d61ed0b6c8174d6a859e18
Cher <red_phoenix417@yahoo.com>
2019-06-12 01:24:55 +0000

    Added a hover feature that darkened the tweets container and link buttons on the footer of each tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function(){
+
+
+
+$('article').on('mouseover', function(){
+  $(this).find('.flag').show();
+});
+
+$('article').on('mouseout', function(){
+  $(this).find('.flag').hide();
+});
+
+}):
